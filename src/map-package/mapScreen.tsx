e8a263ef044dae84cde6beb1c5f9cd2b464e7906
ariel5601 <118356155+ariel5601@users.noreply.github.com>
2024-05-05 12:01:26 +0300

    adding custom routes

--- src/map-package/mapScreen.tsx
+++ src/map-package/mapScreen.tsx
@@ -3,11 +3,34 @@ import {Alert, Platform, SafeAreaView} from 'react-native';
 import {WithNavigation} from '../common';
 import {AppButton} from '../components/atom/appButton/appButton';
 import {mapScreenStyle, mapStyle} from './mapScreenStyle';
-import MapView, {Marker} from 'react-native-maps';
+import MapView, {Marker , Polyline} from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 import {PermissionsAndroid} from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
 
 type MapScreenProps = {} & WithNavigation;
+
+const routeCoordinates = [
+  { latitude: 32.047215, longitude: 34.760438 }, // Start point
+  { latitude: 32.051396, longitude: 34.761771 },  // Waypoint 1
+  { latitude: 32.050829, longitude: 34.748971 },  // Waypoint 2
+  { latitude: 32.068859, longitude: 34.769398 }, // End point (return to start)
+];
+
+const routeCoordinates2 = [
+  { latitude: 32.504717,  longitude: 34.912955 }, // Start point
+  { latitude: 32.503121, longitude: 34.905046 },  // Waypoint 1
+  { latitude: 32.511429, longitude: 34.897651 },  // Waypoint 2
+  { latitude: 32.514716, longitude: 34.897839 }, // End point (return to start)
+];
+const routeCoordinates3 = [
+  { latitude: 32.489999,  longitude: 34.903373 }, // Start point
+  { latitude: 32.502926, longitude: 34.904756 },  // Waypoint 1
+  { latitude: 32.506899, longitude: 34.903635 },  // Waypoint 2
+  { latitude: 32.518878, longitude: 34.907186 }, // End point (return to start)
+];
+
 
 export const MapScreen: React.FC<MapScreenProps> = props => {
   const [location, setLocation] = useState<{
@@ -81,6 +104,9 @@ export const MapScreen: React.FC<MapScreenProps> = props => {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
+        showsUserLocation={true}
+        followsUserLocation={true}
+        
         customMapStyle={mapStyle}>
         {location && (
           <Marker
@@ -92,7 +118,37 @@ export const MapScreen: React.FC<MapScreenProps> = props => {
             description={'This is the current location'}
           />
         )}
+      
+        {/* Display a Polyline representing the custom route */}
+        <Polyline
+          coordinates={routeCoordinates}
+          strokeWidth={4}
+          strokeColor="#00f"
+        />
+        <Polyline
+          coordinates={routeCoordinates2}
+          strokeWidth={4}
+          strokeColor="#00008b"
+        />
+        <Polyline
+          coordinates={routeCoordinates3}
+          strokeWidth={4}
+          strokeColor="#008000"
+        />
+
+
       </MapView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
